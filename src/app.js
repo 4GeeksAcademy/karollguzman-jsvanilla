6eace5dc0df5fdacd54647e834389dee0d1cfca1
Karoll Guzmán <karolldguzman@gmail.com>
2024-11-16 11:34:20 +0000

    mejorando

--- src/app.js
+++ src/app.js
@@ -7,20 +7,25 @@ import "./assets/img/4geeks.ico";
 
 
   window.onload = function() {
-    const domain = generateDomain();
-    console.log(domain);
+    const domains = generateAllDomains();
+    domains.forEach(domain => console.log(domain));
   };
 
 //write your code here
-function generateDomain() {
+function generateAllDomains() {
   let pronoun = ['the','our', 'my', 'your', 'his'];
   let adj = ['great', 'big', 'small', 'fast', 'slow'];
   let noun = ['jogger', 'racoon', 'dog', 'cat', 'fish'];
 
-  let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
-  let randomAdj = adj[Math.floor(Math.random() * adj.length)];
-  let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  let domains = [];
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        domains.push(`${p}${a}${n}.com`);
+      }
+    }
+  }
 
-  return `${randomPronoun}${randomAdj}${randomNoun}.com`;
+  return domains
 }
 
