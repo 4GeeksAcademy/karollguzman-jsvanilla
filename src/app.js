eb640583711394780b351a81b937a5698b3e23ea
Karoll Guzmán <karolldguzman@gmail.com>
2024-11-16 01:53:39 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -5,9 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  let number = 10;
-  console.log("Hello Rigo from the console!");
-  console.log("Hello Spain 91", number);
-};
+
+  window.onload = function() {
+    const domain = generateDomain();
+    console.log(domain);
+  };
+
+//write your code here
+function generateDomain() {
+  let pronoun = ['the','our', 'my', 'your', 'his'];
+  let adj = ['great', 'big', 'small', 'fast', 'slow'];
+  let noun = ['jogger', 'racoon', 'dog', 'cat', 'fish'];
+
+  let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  return `${randomPronoun}${randomAdj}${randomNoun}.com`;
+}
+
